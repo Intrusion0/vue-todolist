26e37590b7467663256e69050ace2c68dbac22b7
Mario <lombardomario01@gmail.com>
2021-11-12 15:24:04 +0100

    To do list completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+/* TODOLIST
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+*/
 var myAppList = new Vue({
     el: '#appList',
     data: {
+        newTask: {
+            text: '',
+            done: false
+        },
         tasks: [
             {
                 text: 'Fare la spesa',
@@ -29,7 +38,15 @@ var myAppList = new Vue({
         ]
     },
     methods: {
-
+        addTask() {
+            if (this.newTask.text.length > 3) {
+                this.tasks.push(this.newTask);
+                this.newTask = {};
+            }
+        },
+        deleteTask(index) {
+            this.tasks.slice(index, 1)
+        }
     },
 });
 
